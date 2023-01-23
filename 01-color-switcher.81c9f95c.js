6ccd7cdf19d2a6a3d83aa9a22ff290fcc2b21724
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-01-23 22:10:04 +0000

    Deploying to gh-pages from @ asiaostrowska55/goit-js-hw-11@4ed83a7bd45266245d4a3d19d64385a9d5ccd389 🚀

---
+++ 01-color-switcher.81c9f95c.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=document.querySelector("body");a.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,a.disabled=!1,e=setInterval((function(){d.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),a.addEventListener("click",(function(){t.disabled=!1,a.disabled=!0,clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.81c9f95c.js.map
